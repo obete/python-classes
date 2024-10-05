65938e51de78e6817405d58c0b72262fd94bfd93
obete <48030976+obete@users.noreply.github.com>
2024-10-05 09:49:29 +0300

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,6 +1,6 @@
 # Assignment 2
 # By Felix Obete, Reg. Number: S24M25/014
-# Write a class named University with Three fundtions Student, Courses and Lectureres
+# Write a class named University with Three functions Student, Courses and Lecturers
 class University:
     def __init__(self, name):
         self.name = name
