c4d9a72477fde713785dc16bffd02091ea21779e
obete <48030976+obete@users.noreply.github.com>
2024-10-05 09:48:46 +0300

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,15 @@
+# Assignment 2
+# By Felix Obete, Reg. Number: S24M25/014
+# Write a class named University with Three fundtions Student, Courses and Lectureres
+class University:
+    def __init__(self, name):
+        self.name = name
+    
+    def students(self):
+        pass
+    
+    def courses(self):
+        pass
+
+    def lecturers(self):
+        pass
